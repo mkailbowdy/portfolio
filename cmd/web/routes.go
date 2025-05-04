983fe2cc17b4fea720f7a5f4a4fc93f9497fe08e
Myhkail Mendoza <myhkail.mendoza@gmail.com>
2025-05-04 21:25:27 +0900

    fix routes

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -7,7 +7,7 @@ func (app *application) routes() *http.ServeMux {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 	// Setup routes and corresponding handlers
-	mux.HandleFunc("GET /", app.home)
+	mux.HandleFunc("GET /{$}", app.home)
 	mux.HandleFunc("GET /shigoto/view/{id}", app.shigotoView)
 	mux.HandleFunc("GET /shigoto/create", app.shigotoCreate)
 	mux.HandleFunc("POST /shigoto/create", app.shigotoCreatePost)
